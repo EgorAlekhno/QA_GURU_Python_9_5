0ec6fea8060146bdaf522070296b4a347b0703c4
Egor Alekhno <alehnoegor@gmail.com>
2023-12-06 15:34:20 +0300

    fix issues

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,15 +1,14 @@
 from selene.support.shared import browser
-from selene import have, by, be, command
+from selene import have, be, command
+import os
 
 
 
 def test_student_registration_form_smoke():
-    # browser.open('https://demoqa.com/automation-practice-form')
     browser.open('/automation-practice-form')
     browser.element('#firstName').type('John')
     browser.element('#lastName').type('Doe')
     browser.element('#userEmail').type('test@test.com')
-    # browser.element('[for="gender-radio-1"]').click()
     browser.all('[for^=gender-radio]').element_by(have.exact_text('Male')).click()
     browser.element('#userNumber').type('7727231131')
     browser.element('#dateOfBirthInput').click()
@@ -22,7 +21,7 @@ def test_student_registration_form_smoke():
     browser.element('.react-datepicker__day--020').click()
     browser.element('#subjectsInput').should(be.blank).type('eng').press_enter()
     browser.all('[for^=hobbies-checkbox-1]').element_by(have.exact_text('Sports')).click()
-    browser.element('#uploadPicture').send_keys('C:/Users/aev32/PycharmProjects/QA_GURU_Python_9_5/tests/resources/sea.jpg')
+    browser.element('#uploadPicture').send_keys(os.path.abspath('resources/sea.jpg'))
     browser.element('#currentAddress').type('Minsk,\nBelarus')
     browser.element('.css-yk16xz-control').click()
     browser.element('#react-select-3-input').type('NCR').press_enter()
